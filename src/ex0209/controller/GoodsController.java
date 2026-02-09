6c29269be14a1c79868167cf2b79d9da4c4fb0e7
hyejinworld <117828146+hyejinworld@users.noreply.github.com>
2026-02-09 15:51:51 +0900

    Update GoodsController.java

--- src/ex0209/controller/GoodsController.java
+++ src/ex0209/controller/GoodsController.java
@@ -3,7 +3,7 @@ package ex0209.controller;
 public class GoodsController {
 	
 	public static void main(String[] args) {
-		System.out.println("test");
+		System.out.println("test 수정22222222");
 	}
      
 }
