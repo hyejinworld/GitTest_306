672e73e037f61864d412935b6164e8d155f0edc7
hyejinworld <amorfati7350@gmail.com>
2026-02-09 15:59:07 +0900

    head origin test

--- src/ex0209/service/TestView.java
+++ src/ex0209/service/TestView.java
@@ -2,10 +2,11 @@ package ex0209.service;
 
 public class TestView {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("출력");
-		
+	public void math(){
 	}
+//		public void select()
+//		{
+//			System.out.println("select");
+//		}
 
 }
