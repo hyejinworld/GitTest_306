ef92e80b2e00e39c0f94830c37f12ae10dfb220a
hyejinworld <117828146+hyejinworld@users.noreply.github.com>
2026-02-10 12:24:23 +0900

    Update GoodsController.java

--- src/ex0209/controller/GoodsController.java
+++ src/ex0209/controller/GoodsController.java
@@ -4,12 +4,6 @@ public class GoodsController {
 	public void orderInsert() {
 		System.out.println("order 상태");
 	}
-//	public static void main(String[] args) {
-//
-//		System.out.println("test 원격수정 ");
-//		System.out.println("test 원격 test 추가함 시나리오 5번 테스트");
-//
-//	}
 	
      
 }
