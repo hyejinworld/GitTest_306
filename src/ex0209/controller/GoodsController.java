8320f2cc6674bbba97db421ae64018f33ac6e2ee
hyejinworld <amorfati7350@gmail.com>
2026-02-10 10:58:47 +0900

    정리 commit

--- src/ex0209/controller/GoodsController.java
+++ src/ex0209/controller/GoodsController.java
@@ -3,9 +3,12 @@ package ex0209.controller;
 public class GoodsController {
 	
 	public static void main(String[] args) {
+
 		System.out.println("test 원격수정 ");
 		System.out.println("test 원격 test 추가함 시나리오 5번 테스트");
+
 	}
 	
      
 }
+
